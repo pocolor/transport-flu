5975efafe4db4fd3ea5e0cb5e96f9d353a574512
pocolor <luvecjr@gmail.com>
2026-05-27 13:29:26 +0200

    refactored core

---
+++ src/tf/core/Logger.cpp
@@ -0,0 +1,73 @@
+#include "Logger.hpp"
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
+#include <vector>
+
+namespace tf {
+    std::shared_ptr<spdlog::logger> Logger::s_engineLogger;
+    std::shared_ptr<spdlog::logger> Logger::s_gameLogger;
+/*
+#define SPDLOG_LEVEL_TRACE 0
+#define SPDLOG_LEVEL_DEBUG 1
+#define SPDLOG_LEVEL_INFO 2
+#define SPDLOG_LEVEL_WARN 3
+#define SPDLOG_LEVEL_ERROR 4
+#define SPDLOG_LEVEL_CRITICAL 5
+#define SPDLOG_LEVEL_OFF 6
+*/
+
+    void Logger::init() {
+        constexpr spdlog::level::level_enum logLevels[] = {
+            spdlog::level::trace,
+            spdlog::level::debug,
+            spdlog::level::info,
+            spdlog::level::warn,
+            spdlog::level::err,
+            spdlog::level::critical,
+        };
+
+        constexpr spdlog::level::level_enum logLevel = logLevels[SPDLOG_ACTIVE_LEVEL];
+
+        auto engineConsoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        engineConsoleSink->set_level(spdlog::level::trace);
+        engineConsoleSink->set_pattern("%^[%T.%e] [%n] [%l] %s:%# %!(): %v%$");
+
+        auto engineFileSink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+            "logs/engine.log",
+            1024 * 1024 * 5,
+            3);
+        engineFileSink->set_level(spdlog::level::info);
+        engineFileSink->set_pattern("[%Y-%m-%d %T] [%l] %v");
+
+        std::vector<spdlog::sink_ptr> engineSinks = { engineConsoleSink, engineFileSink };
+        s_engineLogger = std::make_shared<spdlog::logger>(
+            "ENGINE",
+            engineSinks.begin(),
+            engineSinks.end());
+
+        s_engineLogger->set_level(logLevel);
+        s_engineLogger->flush_on(logLevel);
+
+        auto gameConsoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        gameConsoleSink->set_level(spdlog::level::trace);
+        gameConsoleSink->set_pattern("%^[%T.%e] [%n] [%l] %s:%# %!(): %v%$");
+
+        auto gameFileSink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+            "logs/client.log",
+            1024 * 1024 * 5,
+            3);
+        gameFileSink->set_level(spdlog::level::info);
+        gameFileSink->set_pattern("[%Y-%m-%d %T] [%l] %v");
+
+        std::vector<spdlog::sink_ptr> gameSinks = { gameConsoleSink, gameFileSink };
+        s_gameLogger = std::make_shared<spdlog::logger>(
+            "GAME",
+            gameSinks.begin(),
+            gameSinks.end());
+
+        s_gameLogger->set_level(logLevel);
+        s_gameLogger->flush_on(logLevel);
+    }
+}
---
+++ src/tf/core/Types.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <glm/glm.hpp>
+
+namespace tf {
+    using i8 = int8_t;
+    using i16 = int16_t;
+    using i32 = int32_t;
+    using i64 = int64_t;
+    using u8 = uint8_t;
+    using u16 = uint16_t;
+    using u32 = uint32_t;
+    using u64 = uint64_t;
+    using f32 = float;
+    using f64 = double;
+
+    using str = std::string;
+
+    using vec2 = glm::vec2;
+    using vec3 = glm::vec3;
+    using vec4 = glm::vec4;
+    using mat3 = glm::mat3;
+    using mat4 = glm::mat4;
+    using quat = glm::quat;
+
+    using RendererID = i32;
+}
