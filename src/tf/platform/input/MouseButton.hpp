08ccfacaf7019598c5fcd1454352e58a04775e2e
pocolor <luvecjr@gmail.com>
2026-05-27 17:27:03 +0200

    changed vars to match GLFW macros

--- src/tf/platform/input/MouseButton.hpp
+++ src/tf/platform/input/MouseButton.hpp
@@ -2,20 +2,20 @@
 
 #include "tf/core.hpp"
 
-// borrowed from glfw3.h
+#include <GLFW/glfw3.h>
 
 namespace tf::mouse_button {
-    constexpr i32 _1 = 0;
-    constexpr i32 _2 = 1;
-    constexpr i32 _3 = 2;
-    constexpr i32 _4 = 3;
-    constexpr i32 _5 = 4;
-    constexpr i32 _6 = 5;
-    constexpr i32 _7 = 6;
-    constexpr i32 _8 = 7;
+    constexpr i32 _1 = GLFW_MOUSE_BUTTON_1;
+    constexpr i32 _2 = GLFW_MOUSE_BUTTON_2;
+    constexpr i32 _3 = GLFW_MOUSE_BUTTON_3;
+    constexpr i32 _4 = GLFW_MOUSE_BUTTON_4;
+    constexpr i32 _5 = GLFW_MOUSE_BUTTON_5;
+    constexpr i32 _6 = GLFW_MOUSE_BUTTON_6;
+    constexpr i32 _7 = GLFW_MOUSE_BUTTON_7;
+    constexpr i32 _8 = GLFW_MOUSE_BUTTON_8;
 
-    constexpr i32 Last = _8;
-    constexpr i32 Left = _1;
-    constexpr i32 Right = _2;
-    constexpr i32 Middle = _3;
+    constexpr i32 Last = GLFW_MOUSE_BUTTON_LAST;
+    constexpr i32 Left = GLFW_MOUSE_BUTTON_LEFT;
+    constexpr i32 Right = GLFW_MOUSE_BUTTON_RIGHT;
+    constexpr i32 Middle = GLFW_MOUSE_BUTTON_MIDDLE;
 }
